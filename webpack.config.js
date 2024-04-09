6f4cfdbceaa31997bbac8529cc50c9824e7e4075
Robpizza <12848142+Robpizza@users.noreply.github.com>
2024-04-09 18:46:28 +0200

    Added WebPack for js bundeling // Sass loader for TailwindCSS // WebPack for local development

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    liveReload: true,
+    hot: true,
+    open: true,
+    static: ['./'],
+  },
+  entry: './src/App.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css', // Output CSS filename
+    }),
+  ],
+});
