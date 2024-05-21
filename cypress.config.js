620c1911c825fb512d2b123fa378135da0373947
s1182276 <119663924+s1182276@users.noreply.github.com>
2024-05-21 14:46:10 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:8030",
+    baseUrl: process.env.CYPRESS_BASE_URL,
     includeShadowDom: true,
   },
 });
