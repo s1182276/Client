54fd84dc461a4bd6e6293d8fa397f329f536d25c
s1182276 <s1182276@student.windesheim.nl>
2024-05-27 21:59:57 +0200

    Build sequentially, remove public folder deps

--- webpack.config.js
+++ webpack.config.js
@@ -56,12 +56,14 @@ module.exports = {
             path: path.join(__dirname, '.client.env')
         }),
         new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: 'index.html'
+            template: './src/index.html',
+            filename: '../index.html'
         }),
         new CopyWebpackPlugin({
             patterns: [
-                { from: 'src/Css/tinymce.min.css', to: 'css' }
+                { from: 'src/Css/tinymce.min.css', to: 'css' },
+                { from: 'src/manifest.json', to: '../manifest.json' },
+                { from: 'lib/jquery-3.7.1.js', to: 'lib/jquery-3.7.1.js' }
             ]
         })
     ]
